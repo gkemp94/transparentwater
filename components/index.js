eeb45cfd975dfcf36826feacfedd1ca5c145fb46
George Kemp <georgekemp94@gmail.com>
2018-06-04 20:56:59 +0100

    Adds Index for Components

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import ListView from './ListView';
+import ItemView from './ItemView';
+import MapView from './MapView';
+import SettingsView from './SettingsView';
+
+export {
+    ListView,
+    ItemView, 
+    MapView,
+    SettingsView,
+}
